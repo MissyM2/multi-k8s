3ffa7fccd66fae2e6e89cbb5e97bed3dea8d1abb
Missy Maloney <mmaloney@healthstarInfo.com>
2020-04-03 13:06:56 -0400

    added complex app

---
+++ server/keys.js
@@ -0,0 +1,11 @@
+// all these environment variables have to be passed
+
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT
+};
