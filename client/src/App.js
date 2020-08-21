3d3fcd1b1a143c299406ef0db127ef1fc4dc9fd9
Missy Maloney <mmaloney@healthstarInfo.com>
2020-08-21 14:39:03 -0400

    added h3

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React - Missy</h1>
+            <h3>Come on, make this thing work</h3>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
