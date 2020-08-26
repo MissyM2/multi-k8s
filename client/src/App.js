c8f1770e940fa54fcaddd06c9bbcff1fb128b37f
Missy Maloney <mmaloney@healthstarInfo.com>
2020-08-26 10:26:06 -0400

    converting to k8s deployment

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React - Missy</h1>
-            <h3>Come on, make this thing work</h3>
+            <h1 className="App-title">Fib Calculator version 2</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
